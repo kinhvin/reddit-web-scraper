7fdba32a5a5da7cb939c4a0e1f6fa2f1aaa09317
kinhvin <kevintruong.code@gmail.com>
2025-10-10 00:32:42 -0400

    adding csv data uploads to google sheets using gspread + added some documentation

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,20 +1,33 @@
 import time
 import httpx
 import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+# Connect to Google Sheets
+scope = ["https://www.googleapis.com/auth/spreadsheets",
+         "https://www.googleapis.com/auth/drive"]
+
+# Set up the client
+creds = ServiceAccountCredentials.from_json_keyfile_name("service_account_creds.json", scope)
+client = gspread.authorize(creds)
+
+# Open the spreadsheet
+sheet = client.open("Reddit Scraping").sheet1
+
+# Set up the Reddit API
 base_url = "https://www.reddit.com"
 endpoint = "/r/10xPennyStocks"
-category = "/hot"
-
+category = "/best"
 url = base_url + endpoint + category + ".json"
 after_post_id = None
-
 dataset = []
 
+# Scrape 500 posts from within the last month
 for _ in range(5):
     params = {
-        "limit": 25,
-        "t": "week", # time unit (hour, day, week, month, year, all)
+        "limit": 100,
+        "t": "month", # time unit (hour, day, week, month, year, all)
         "after": after_post_id
     }
     
@@ -29,5 +42,17 @@ for _ in range(5):
     after_post_id = data["data"]["after"]
     time.sleep(0.5)
     
-    df = pd.DataFrame(dataset)
-    df.to_csv("reddit_data.csv", index=False)
+df = pd.DataFrame(dataset)
+
+# Sanitize the data of instances of list and dict
+for col in df.columns:
+    df[col] = df[col].apply(lambda x: str(x) if isinstance(x, (list, dict)) else x)
+
+# Sanitize the data of any inf or -inf
+df = df.replace([float('inf'), float('-inf')], None).fillna("")
+df.to_csv("reddit_data.csv", index=False)
+
+# Clear the current sheet and upload the new CSV data to it
+sheet.clear()
+sheet.update([df.columns.values.tolist()] + df.values.tolist())
+print(f"Data uploaded to Google Sheets at {sheet.url}")
