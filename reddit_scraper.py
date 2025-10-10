01710c9dee257df0754689102af26a0dd8e88e7a
kinhvin <kevintruong.code@gmail.com>
2025-10-10 01:59:16 -0400

    now adding the ability to scrape multiple subreddits rather than just 1 + added a bit of customization with what category to scrape by

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -17,42 +17,52 @@ sheet = client.open("Reddit Scraping").sheet1
 
 # Set up the Reddit API
 base_url = "https://www.reddit.com"
-endpoint = "/r/10xPennyStocks"
-category = "/best"
-url = base_url + endpoint + category + ".json"
-after_post_id = None
+subreddits = ["10xPennyStocks", "pennystocks", "Pennystock", "PennyStockProfits", "buzztickr"]
+category = input("Enter a valid category (best, hot, new, top, rising): ")
 dataset = []
 
-# Scrape 500 posts from within the last month
-for _ in range(5):
-    params = {
-        "limit": 100,
-        "t": "month", # time unit (hour, day, week, month, year, all)
-        "after": after_post_id
-    }
+# Scrape posts from each subreddit
+for sub in subreddits:
+    url = f"{base_url}/r/{sub}/{category}.json"
+    after_post_id = None
+    print(f"\nScraping r/{sub}...")
     
-    response = httpx.get(url, params=params)
-    print(f"fetching {response.url}...")
-    if response.status_code != 200:
-        raise Exception("Failed to fetch data")
-    
-    data = response.json()
-    dataset.extend(rec["data"] for rec in data["data"]["children"])
-    
-    after_post_id = data["data"]["after"]
-    time.sleep(0.5)
+    # Scrape posts from respective subreddit
+    for _ in range(5):
+        
+        if sub == "buzztickr":
+            params = {
+                "limit": 1,
+                "t": "week", # time unit (hour, day, week, month, year, all)
+                "after": after_post_id
+            }
+        
+        else:
+            params = {
+                "limit": 20,
+                "t": "month", # time unit (hour, day, week, month, year, all)
+                "after": after_post_id
+            }
+        
+        response = httpx.get(url, params=params)
+        print(f"fetching {response.url}...")
+        if response.status_code != 200:
+            raise Exception("Failed to fetch data")
+        
+        data = response.json()
+        dataset.extend(rec["data"] for rec in data["data"]["children"])
+        
+        after_post_id = data["data"]["after"]
+        
+        # Add a delay to avoid 429 errors
+        time.sleep(0.5)
     
 df = pd.DataFrame(dataset)
 
-# Sanitize the data of instances of list and dict
-for col in df.columns:
-    df[col] = df[col].apply(lambda x: str(x) if isinstance(x, (list, dict)) else x)
-
-# Sanitize the data of any inf or -inf
-df = df.replace([float('inf'), float('-inf')], None).fillna("")
-df.to_csv("reddit_data.csv", index=False)
+# Only include relevant columns in the dataset
+df = df[["subreddit", "title", "ups", "downs", "num_comments", "url", "author"]]
 
 # Clear the current sheet and upload the new CSV data to it
 sheet.clear()
 sheet.update([df.columns.values.tolist()] + df.values.tolist())
-print(f"Data uploaded to Google Sheets at {sheet.url}")
+print(f"\nData uploaded to Google Sheets at {sheet.url}")
